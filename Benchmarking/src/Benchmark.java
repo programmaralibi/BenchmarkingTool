4bf49a07f90f7e9cda4e1870eaa1b3c0da73ab2a
amber <amberras@buffalo.edu>
2014-05-18 13:13:18 -0400

    Adding Original Dacapo jar and README

--- Benchmarking/src/Benchmark.java
+++ Benchmarking/src/Benchmark.java
@@ -1,32 +1,48 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 
 /*
  * Parameters : 
- * 1) MinMemory
- * 2) MaxMemory
- * 3) Option1
- * 4) Option2
- * 
- * Option1 :
+ * 1) Param0 : MinMemory
+ * 2) Param1 : MaxMemory
+ * 3) Param2 : BenchmarkName
+ * 		   1 : h2
+ * 		   2 : h2
+ * 4) Param3 : Number Of executions
+ * 5) Param4 : Size of benchmark
+ * 		   1 : small
+ * 		   2 : large
+ * 	 default : medium  
  *  
  */
 public class Benchmark {
+	
+	static PrintWriter resultFile = null;
+	static String JAVOLUTION_DACAPO = "dacapo.jar"; 
+	static String JAVA_DACAPO = "dacapo_original.jar"; 
+
 	public static void main(String args[]) {
 		System.out.println("Running the test for ");
 		String minMemory = "-Xms" + args[0] + "M";
 		String maxMemory = "-Xmx" + args[1] + "M";
 		Integer option1 = Integer.parseInt(args[2]);
 		Integer option2 = Integer.parseInt(args[3]);
+		Integer option3 = null;
+		if(args.length > 4) {
+			option3 = Integer.parseInt(args[4]);
+		}
+		
 		File file = new File("Result");
-		FileWriter resultFile = null;
 		try {
-			resultFile = new FileWriter(file, true);
+			resultFile = new PrintWriter(new FileWriter(file, true));
 			resultFile.write("******************************************\n");
 			resultFile.write("-----------------START-----------------\n");
 			resultFile.flush();
@@ -34,6 +50,7 @@ public class Benchmark {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
+		
 		
 		List<String> commandList = new ArrayList<String>();
 		commandList.add("java");
@@ -46,35 +63,67 @@ public class Benchmark {
 			case 1 : commandList.add("h2"); break;
 			case 2 : commandList.add("h2"); break;
 		}
-		String[] commandArray = new String[commandList.size()];
 		
+		switch (option3) {
+			case 1 : commandList.add("-s"); commandList.add("small"); break;
+			case 2 : commandList.add("-s"); commandList.add("large"); break;
+		}
+
+		String[] commandArray = new String[commandList.size()];
+		commandArray = commandList.toArray(commandArray);
+		executeBenchmark(commandArray, option2);
+		
+		int index = commandList.indexOf(JAVOLUTION_DACAPO);
+		commandList.remove(JAVOLUTION_DACAPO);
+		commandList.add(index, JAVA_DACAPO);
+
+		commandArray = commandList.toArray(commandArray);
+		executeBenchmark(commandArray, option2);
+
+		resultFile.close();
+
+	}
+	
+	static void executeBenchmark(String[] commandArray, int numberOfExecution) {
 		try {
-			for(int numberOfRuns=0; numberOfRuns<option2; numberOfRuns++) {
+			for(int numberOfRuns=0; numberOfRuns<numberOfExecution; numberOfRuns++) {
 				Long startTime = System.currentTimeMillis();
 				Process process;
-				ProcessBuilder pBuilder = new ProcessBuilder(commandList.toArray(commandArray));
+				ProcessBuilder pBuilder = new ProcessBuilder(commandArray);
 				//pBuilder.redirectOutput(file);
 				process = pBuilder.start();
+
 				InputStream stream = process.getInputStream();
-				int c;
-				while ((c = stream.read()) != -1) {
-					resultFile.write(c);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+				String line;
+				while ((line = reader.readLine()) != null) {
+					System.out.println(line);
+					resultFile.write(line+"\n");
+					resultFile.flush();
 				}
 				stream = process.getErrorStream();
-				while ((c = stream.read()) != -1) {
-					resultFile.write(c);
+				
+				reader = new BufferedReader(new InputStreamReader(stream));
+				while ((line = reader.readLine()) != null) {
+					System.out.println(line);
+					resultFile.write(line+"\n");
+					resultFile.flush();
 				}
 
+				long timeTaken = System.currentTimeMillis() - startTime;
+				StringBuilder commandStr = new StringBuilder("");
+				for(int index=0; index<commandArray.length; index++) {
+					commandStr.append(" "+commandArray[index]);
+				}
+				resultFile.write("\n Time taken for executing : " + timeTaken + " command :---\n"+commandStr.toString());
 				process.waitFor();
-				resultFile.write("\n Time taken for executing : " + (System.currentTimeMillis() - startTime) + "command :---\n"+commandList.toString() );
 			}
 			resultFile.write("\n-----------------END----------------- \n\n\n\n");
 			resultFile.flush();
-			resultFile.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
